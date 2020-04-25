888e667692ce30e88de45e3731ebaf99eb5944ac
pomelo072 <54461099+pomelo072@users.noreply.github.com>
2020-04-25 14:11:09 +0800

    4.25 14.11

---
+++ Programs/A_Main.py
@@ -0,0 +1,9 @@
+from Programs import Data_Pretreatment as DP
+from Programs import File_Manager as FM
+from Programs import Model_Build as MB
+
+
+raw_data_day, raw_data_year, raw_data_Basic = FM.Input_Raw_File('X', 'Y', 'Z')
+
+
+
---
+++ Programs/A_Test.py
@@ -0,0 +1,1 @@
+from Programs import File_Manager as FM
