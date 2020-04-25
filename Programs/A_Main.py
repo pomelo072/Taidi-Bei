da7a2a12ea1ec19ff4f59f7ad9bb26c183292636
pomelo072 <54461099+pomelo072@users.noreply.github.com>
2020-04-25 14:59:59 +0800

    4.25 15.00

--- Programs/A_Main.py
+++ Programs/A_Main.py
@@ -3,7 +3,16 @@ from Programs import File_Manager as FM
 from Programs import Model_Build as MB
 
 
-raw_data_day, raw_data_year, raw_data_Basic = FM.Input_Raw_File('X', 'Y', 'Z')
+raw_data_day, raw_data_year, raw_data_basic = FM.Input_Raw_File('X', 'Y', 'Z')
+
+p_ZBGJ_WFLR, p_YYGJ, p_LCSY, p_JZC, p_XJJE = DP.Day_Data_Extraction(raw_data_day)
+
+total_GB, rate_SRZZ = DP.Year_Data_Extraction(raw_data_year)
+
+year_listed, snew_share, YZ_YJ, CZ_CD = Basic_Data_Extraction(raw_data_basic)
+
+data_list = [p_ZBGJ_WFLR, p_YYGJ, p_LCSY, p_JZC, p_XJJE, total_GB, rate_SRZZ, year_listed, snew_share, YZ_YJ, CZ_CD]
 
 
-
+for i in data_list:
+    pass
