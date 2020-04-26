e172eb3e5dd8afecd841db7e512cc1e86c74ed93
pomelo072 <54461099+pomelo072@users.noreply.github.com>
2020-04-26 17:09:52 +0800

    4.26 17.00

--- Programs/A_Main.py
+++ Programs/A_Main.py
@@ -5,19 +5,22 @@ from Programs import File_Manager as FM
 # 模型预测模块
 from Programs import Model_Build as MB
 
-
 if __name__ == '__main__':
     # 导入原始数据
-    raw_data_day, raw_data_year, raw_data_basic = FM.Input_Raw_File('X', 'Y', 'Z')
+    raw_data_day, raw_data_year, raw_data_basic = FM.Input_Raw_File(u'./Data/Day.csv',
+                                                                    u'./Data/Year.csv',
+                                                                    u'./Data/Basic.csv')
     # 提取日数据项
-    data_day = DP.Day_Data_Extraction(raw_data_day)
+    # data_day = DP.Day_Data_Extraction(raw_data_day)
+    # data_day.to_csv('./data_day.csv', encoding='GBK')
     # 提取年数据项
-    data_year = DP.Year_Data_Extraction(raw_data_year)
-    # 提出基础数据项
+    # data_year = DP.Year_Data_Extraction(raw_data_year)
+    # # 提出基础数据项
     data_basic = DP.Basic_Data_Extraction(raw_data_basic)
-    # 数据合并
-    data = DP.Data_Merge(DP.DataMerge(data_day, data_year, '年'), data_basic, '股票编号')
-    # 数据清洗
-    data = DP.Data_Wash(data)
-    # 数据标准化
-    data = DP.Data_Standardization(data)
+    data_basic.to_csv('./data_day.csv', encoding='GBK')
+    # # 数据合并
+    # data = DP.Data_Merge(DP.DataMerge(data_day, data_year, '年'), data_basic, '股票编号')
+    # # 数据清洗
+    # data = DP.Data_Wash(data)
+    # # 数据标准化
+    # data = DP.Data_Standardization(data)
--- Programs/Data_Pretreatment.py
+++ Programs/Data_Pretreatment.py
@@ -3,16 +3,29 @@ import numpy as np
 
 
 # data_list = [p_ZBGJ_WFLR, p_YYGJ, p_TTM, p_JZC, p_XJJE, total_GB, rate_SRZZ, year_listed, snew_share, YZ_YJ, CZ_CD]
+# 日数据提取
 def Day_Data_Extraction(data):
-    group_data = data.groupby(by=['股票编号', '年', '月'])[['每股盈余公积金', '每股收益TTM值', '每股净资产',
-                                                      '每股现金流量净额']].agg(lambda x: x.mode()).reset_index()
-    return group_data
+    # 分组
+    group_data = data.groupby(by=['股票编号', '年', '月'])
+    # 求均值
+    temp_data = group_data['收盘价'].agg(np.mean).reset_index()
+    # 取1月1日前1个月数据
+    fanal_data = temp_data.loc[temp_data['月'] == 12, :]
+    return fanal_data
 
 
 def Year_Data_Extraction(data):
-    group_data = data.groupby(by=['股票编号', '年份（年份）'])[['实收资本(或股本)', '营业总收入同必增长(%)']].agg()
-    return None
+    right_data = data.loc[:, ['股票编号', '年份（年末）', '每股现金流量净额(元/股)', '实收资本(或股本)',
+                              '每股收益(期末摊薄，元/股)', '每股净资产(元/股)', '营业总收入同必增长(%)']]
+    # 求ZBGJ和WFLR的和
+    left_data = data.loc[:, ['股票编号', '年份（年末）', '每股资本公积(元/股)', '每股未分配利润(元/股)']]
+    left_data['每股资本公积(元/股)+每股未分配利润(元/股)'] = left_data[['每股资本公积(元/股)', '每股未分配利润(元/股)']].sum(axis=1)
+    # 合并两表
+    fanal_data = pd.merge(left_data, right_data, on=['股票编号', '年份（年末）'])
+    return fanal_data
 
+
+# 次新股判断
 def Is_snew_share(x):
     if '次新股' in x:
         return 1
@@ -20,6 +33,7 @@ def Is_snew_share(x):
         return 0
 
 
+# 预增减判断
 def Is_YZ_YJ(x):
     if '预增' in x:
         return 1
@@ -29,6 +43,7 @@ def Is_YZ_YJ(x):
         return 0
 
 
+# 超涨跌判断
 def Is_CZ_CD(x):
     if '超涨' in x:
         return 1
@@ -38,3 +53,12 @@ def Is_CZ_CD(x):
         return 0
 
 
+# 基础数据提取
+def Basic_Data_Extraction(data):
+    group_data = data.groupby(by='股票编号')
+    fanal_data = data.loc[:, ['股票编号', '上市年限']]
+    # 以下错误
+    fanal_data['次新股'] = group_data['所属概念板块'].agg(Is_snew_share)
+    fanal_data['预增或预减'] = group_data['所属概念板块'].agg(Is_YZ_YJ)
+    fanal_data['超涨或超跌'] = group_data['所属概念板块'].agg(Is_CZ_CD)
+    return fanal_data
